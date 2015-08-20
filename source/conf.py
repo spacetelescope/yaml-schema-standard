43433264a512e60b38fddf37949842bc981d91aa
Erik M. Bray <embray@stsci.edu>
2015-08-20 15:04:03 -0400

    Remove references to currently nonexistent logos

--- source/conf.py
+++ source/conf.py
@@ -130,7 +130,7 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = '_static/logo.ico'
+#html_favicon = '_static/logo.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -236,7 +236,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = '_static/logo.pdf'
+#latex_logo = '_static/logo.pdf'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
